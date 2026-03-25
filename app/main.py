95c3377ab704e0c3ed5697154b8f201cb895d753
ZakGitStudent <Zakbour54@gmail.com>
2026-03-25 15:27:24 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,12 +1,15 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 
 app = FastAPI()
 
+@app.get("/myip")
+def get_my_ip(request: Request):
+    return {"client_ip": request.client.host}
+
 @app.get("/")
 def read_root():
-    return { "msg": "Hello!", "v": "0.1" }
+    return {"msg": "Hello!", "v": "0.1"}
 
-
-@app.get("/items/{id}")
-def read_item(item_id: int, q: str = None):
-    return {"id": id, "q": q}
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: str | None = None):
+    return {"item_id": item_id, "q": q}
